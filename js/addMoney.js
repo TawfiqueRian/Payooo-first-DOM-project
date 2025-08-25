3fe97156e281f8d312788088314671eba0b057be
TawfiqueRian <riantoufik897@gmail.com>
2025-08-25 23:47:59 +0600

    cashout page is done

--- js/addMoney.js
+++ js/addMoney.js
@@ -2,6 +2,19 @@ document.getElementById('addMoneyBtn').addEventListener('click', function(e){
     e.preventDefault();
     const availableBalance = parseInt(document.getElementById('available-balance').innerText);
     const addAmount = parseInt(document.getElementById('add-amount').value);
+    const bankAccount = document.getElementById('bank-account').value;
+    const validPin = '1234';
+    const pinNumber = document.getElementById('add-pin').value;
+    if(bankAccount.length !== 11)
+    {
+        alert('Please enter valid Bank Account Number');
+        return;
+    }
+    if(pinNumber !== validPin)
+    {
+        alert('Invalid Pin Number');
+        return;
+    }
     const newAvailableBalance = availableBalance + addAmount;
     document.getElementById('available-balance').innerText = newAvailableBalance;
     document.getElementById('select-bank').value = 'Select bank';
---
+++ js/home.js
@@ -0,0 +1,18 @@
+document.getElementById('card1').addEventListener('click', function(){
+    window.location.href = "./add-money.html";
+})
+document.getElementById('card2').addEventListener('click', function(){
+    window.location.href = "./cashout.html";
+})
+document.getElementById('card3').addEventListener('click', function(){
+    window.location.href = "./add-money.html";
+})
+document.getElementById('card4').addEventListener('click', function(){
+    window.location.href = "./add-money.html";
+})
+document.getElementById('card5').addEventListener('click', function(){
+    window.location.href = "./add-money.html";
+})
+document.getElementById('card6').addEventListener('click', function(){
+    window.location.href = "./add-money.html";
+})
