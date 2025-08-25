7fbe31266a17a77b0327553f0115467c09c25d12
TawfiqueRian <riantoufik897@gmail.com>
2025-08-25 20:17:27 +0600

    home is done half

--- js/script.js
+++ js/script.js
@@ -2,5 +2,18 @@
 
 document.getElementById('loginBtn').addEventListener('click', function(e){
     e.preventDefault();
-    console.log('Login successfully');
+    const mobile = '01711529698';
+    const pin = '1234';
+    const takeMobileNumber = document.getElementById('mobile-number');
+    const mobileNumber = takeMobileNumber.value;
+    const takePinNumber = document.getElementById('pin-number');
+    const pinNumber = takePinNumber.value;
+    if(mobileNumber === mobile && pinNumber === pin)
+    {
+        window.location.href = "./home.html";
+    }
+    else
+    {
+        alert('Invalid credentials');
+    }
 })
