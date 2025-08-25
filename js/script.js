49df1af50ebcb5231ffc80003b0df27f8f29df9a
TawfiqueRian <riantoufik897@gmail.com>
2025-08-25 17:29:36 +0600

    index.html is done

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// console.log("Script is connencted")
+
+document.getElementById('loginBtn').addEventListener('click', function(e){
+    e.preventDefault();
+    console.log('Login successfully');
+})
